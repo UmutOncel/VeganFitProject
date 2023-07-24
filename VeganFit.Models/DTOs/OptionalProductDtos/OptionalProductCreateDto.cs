5335cd1e4c6c8ba06fc9fbf8bfdf1c580b0b4bb7
KahveciFurkan <furkankahveci21@gmail.com>
2023-07-24 11:30:53 +0300

    Eklentiler eklendi.

--- VeganFit.Models/DTOs/OptionalProductDtos/OptionalProductCreateDto.cs
+++ VeganFit.Models/DTOs/OptionalProductDtos/OptionalProductCreateDto.cs
@@ -1,6 +1,12 @@
-﻿namespace VeganFit.Models.DTOs.OptionalProductDtos
+﻿using VeganFit.Core.Enums;
+
+namespace VeganFit.Models.DTOs.OptionalProductDtos
 {
     public class OptionalProductCreateDto
     {
+        public string CreatedBy { get; set; }
+        public State State => State.Created;
+
+        public DateTime CreateDate = DateTime.Now;
     }
 }
