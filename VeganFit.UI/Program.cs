19690cc43ff18f9535e478b61e7ea2f298b118e7
KahveciFurkan <furkankahveci21@gmail.com>
2023-07-26 21:30:32 +0300

    SetProduct

--- VeganFit.UI/Program.cs
+++ VeganFit.UI/Program.cs
@@ -27,7 +27,7 @@ namespace VeganFit.UI
             var form9 = EFContextForm.EFContextForm.ConfigureServices<UserAddNewProductForm>();
             
 
-            Application.Run(form8);
+            Application.Run(form);
         }
     }
 }
